6af43e9711c8d8508839b20f1c89bc71f1bec7ac
Wiktor Janecki <j.wiktor05@interia.pl>
2020-12-29 16:14:08 +0100

    Delete compile script

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p obj
-mv ./obj/*.o ./
-g++ -c src/*.cpp src/*/*.cpp -std=c++14 -m64 -g -Wall -I include
-g++ *.o -o bin/debug/main -lsfml-graphics -lsfml-window -lsfml-system
-mv *.o ./obj
-./bin/debug/main
