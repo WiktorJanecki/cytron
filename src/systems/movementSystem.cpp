aac6e3ac877df41a951f28919d0bc4ce45fc4c48
Wiktor Janecki <j.wiktor05@interia.pl>
2021-01-08 16:11:53 +0100

    Add 8 directional movement system

--- src/systems/movementSystem.cpp
+++ src/systems/movementSystem.cpp
@@ -53,17 +53,29 @@ void MovementSystem::handleEvent(sf::Event event){
 }
 
 void MovementSystem::calculate(){
-    if(m_w){
+    if(m_w && !m_s && !m_a && !m_d){
         m_direction = sf::Vector2f(0,-1.f);
     }
-    else if(m_s){
+    else if(m_s && !m_w && !m_a && !m_d){
         m_direction = sf::Vector2f(0,1.f);
     }
-    else if(m_a){
+    else if(m_a && !m_w && !m_s && !m_d){
         m_direction = sf::Vector2f(-1.f,0);
     }
-    else if(m_d){
+    else if(m_d && !m_w && !m_s && !m_a){
         m_direction = sf::Vector2f(1.f,0);
+    }
+    else if(m_w && m_a && !m_s && !m_d){
+        m_direction = sf::Vector2f(-1.f,-1.f);
+    }
+    else if(m_w && m_d && !m_s && !m_a){
+        m_direction = sf::Vector2f(1.f,-1.f);
+    }
+    else if(m_d && m_s && !m_w && !m_a){
+        m_direction = sf::Vector2f(1.f,1.f);
+    }
+    else if(m_s && m_a && !m_w && !m_d){
+        m_direction = sf::Vector2f(-1.f,1.f);
     }
     else{
         m_direction = sf::Vector2f(0.f,0.f);
@@ -74,15 +86,24 @@ void MovementSystem::calculate(){
         if(movComp != nullptr && rectComp != nullptr){
             float dt = TimeManager::getDT();
             float speed = 500.f; 
-            float maxSpeed = 50.f;
-            float friction = speed;
+            float maxSpeed = 200.f;
+            float friction = speed/1.5f;
 
             movComp->setAcceleration(sf::Vector2f(m_direction.x * speed, m_direction.y * speed));
 
             movComp->increaseVelocity(movComp->getAcceleration().x*dt,movComp->getAcceleration().y*dt);
-            
+             
             if(movComp->getVelocity().x > 0){
-                //apply friction
+                movComp->increaseVelocity(dt * -friction,0.f);
+            }
+            if(movComp->getVelocity().x < 0){
+                movComp->increaseVelocity(dt * friction, 0.f);
+            }
+            if(movComp->getVelocity().y > 0){
+                movComp->increaseVelocity(0.f, dt * -friction);
+            }
+            if(movComp->getVelocity().y < 0){
+                movComp->increaseVelocity(0.f, dt * friction);
             }
 
             if(movComp->getVelocity().x > maxSpeed){
@@ -97,7 +118,7 @@ void MovementSystem::calculate(){
             if(movComp->getVelocity().y < -maxSpeed){                                                                            
                 movComp->setVelocity(sf::Vector2f(movComp->getVelocity().x,-maxSpeed));                                          
             }
-
+    
             rectComp->getRectangleShape()->move(movComp->getVelocity().x * dt, dt * movComp->getVelocity().y);
         }
     }
